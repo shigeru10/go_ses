98bad14845ab3725a4ed71b20eb4e7171841337f
shigeru10 <shiiiiigeeeee10@gmail.com>
2020-01-29 21:50:04 +0900

    modify main.go

--- main.go
+++ main.go
@@ -3,41 +3,59 @@ package main
 import (
 	"fmt"
 
+	//go get -u github.com/aws/aws-sdk-go
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ses"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 )
 
 const (
-	// Sender is "From" address.
+	// Replace sender@example.com with your "From" address.
+	// This address must be verified with Amazon SES.
 	Sender = "sender@example.com"
 
-	// Recipient is "To" address.
+	// Replace recipient@example.com with a "To" address. If your account
+	// is still in the sandbox, this address must be verified.
 	Recipient = "recipient@example.com"
-	ConfigurationSet = "ConfigSet"
-	AwsRegion = "us-west-2"
-	Subject = "Amazon SES Test (AWS SDK for Go)"
-	HTMLBody =  "<h1>Amazon SES Test Email (AWS SDK for Go)</h1><p>This email was sent with " +
-	"<a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the " +
-	"<a href='https://aws.amazon.com/sdk-for-go/'>AWS SDK for Go</a>.</p>"
 
+	// Specify a configuration set. If you do not want to use a configuration
+	// set, comment out the following constant and the
+	// ConfigurationSetName: aws.String(ConfigurationSet) argument below
+	// ConfigurationSet = "ConfigSet"
+
+	// Replace us-west-2 with the AWS Region you're using for Amazon SES.
+	AwsRegion = "us-west-2"
+
+	// The subject line for the email.
+	Subject = "Amazon SES Test (AWS SDK for Go)"
+
+	// The HTML body for the email.
+	HtmlBody = "<h1>Amazon SES Test Email (AWS SDK for Go)</h1><p>This email was sent with " +
+		"<a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the " +
+		"<a href='https://aws.amazon.com/sdk-for-go/'>AWS SDK for Go</a>.</p>"
+
+	//The email body for recipients with non-HTML email clients.
 	TextBody = "This email was sent with Amazon SES using the AWS SDK for Go."
 
+	// The character encoding for the email.
 	CharSet = "UTF-8"
 )
 
 func main() {
+
+	// Create a new session and specify an AWS Region.
 	sess, err := session.NewSession(&aws.Config{
-		Region:aws.String(AwsRegion)},
+		Region: aws.String(AwsRegion)},
 	)
 
+	// Create an SES client in the session.
 	svc := ses.New(sess)
 
+	// Assemble the email.
 	input := &ses.SendEmailInput{
 		Destination: &ses.Destination{
-			CcAddresses: []*string{
-			},
+			CcAddresses: []*string{},
 			ToAddresses: []*string{
 				aws.String(Recipient),
 			},
@@ -46,35 +64,38 @@ func main() {
 			Body: &ses.Body{
 				Html: &ses.Content{
 					Charset: aws.String(CharSet),
-					Data: aws.String(HTMLBody),
+					Data:    aws.String(HtmlBody),
 				},
 				Text: &ses.Content{
 					Charset: aws.String(CharSet),
-					Data: aws.String(TextBody),
+					Data:    aws.String(TextBody),
 				},
 			},
 			Subject: &ses.Content{
 				Charset: aws.String(CharSet),
-				Data: aws.String(Subject),
+				Data:    aws.String(Subject),
 			},
 		},
 		Source: aws.String(Sender),
-		ConfigurationSetName: aws.String(ConfigurationSet),
+		// Comment or remove the following line if you are not using a configuration set
+		// ConfigurationSetName: aws.String(ConfigurationSet),
 	}
 
+	// Attempt to send the email.
 	result, err := svc.SendEmail(input)
 
+	// Display error messages if they occur.
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case ses.ErrCodeMessageRejected:
-					fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
 			case ses.ErrCodeMailFromDomainNotVerifiedException:
-					fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
 			case ses.ErrCodeConfigurationSetDoesNotExistException:
-					fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
 			default:
-					fmt.Println(aerr.Error())
+				fmt.Println(aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
@@ -87,4 +108,3 @@ func main() {
 	fmt.Println("Email Sent!")
 	fmt.Println(result)
 }
-
